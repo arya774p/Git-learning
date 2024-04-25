7c6827007ed437a568944dc0558e0125da550b41
Prerna Arya <arya774p@gmail.com>
2024-04-25 14:32:20 +0530

    added hello.c file

---
+++ Hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    float pi = 3.14;
+    printf("Age is: %f \n", pi);
+    return 0; 
+}                                                                                                           
